cd2798572a3a8085a38695457ab39c60c17951b9
jiemooschina <460706836@qq.com>
2017-04-19 19:36:48 +0800

    C3

---
+++ Chapter2/C3.py
@@ -0,0 +1,40 @@
+import pygame
+import sys
+import random
+
+from pygame.locals import *
+
+pygame.init()
+pygame.display.set_caption("Drawing Rectangles")
+
+screen = pygame.display.set_mode((600,500))
+
+pos_x = 300
+pos_y = 250
+
+vel_x = 2
+vel_y = 1
+color = 255,255,0
+while True:
+    for event in pygame.event.get():
+        if event.type in (QUIT, KEYDOWN):
+            sys.exit()
+
+    screen.fill((0,0,200))
+
+    pos_x += vel_x
+    pos_y += vel_y
+
+
+    if pos_x > 500 or pos_x < 0:
+        vel_x = -vel_x
+        color = [random.randint(0,255) for _ in range(3)]
+    if pos_y > 400 or pos_y < 0:
+        vel_y = -vel_y
+        color = [random.randint(0,255) for _ in range(3)]
+
+    width = 0
+    pos = pos_x,pos_y,100,100
+    pygame.draw.rect(screen, color, pos, width)
+
+    pygame.display.update()
